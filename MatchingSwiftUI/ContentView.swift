d1b6120d949419dee3073875265a52dda91eae5f
y-arimura1222 <y.arimura.carecon@gmail.com>
2022-11-13 14:58:49 +0900

    Initial Commit

---
+++ MatchingSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MatchingSwiftUI
+//
+//  Created by 有村優希 on 2022/11/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MatchingSwiftUI/MatchingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchingSwiftUIApp.swift
+//  MatchingSwiftUI
+//
+//  Created by 有村優希 on 2022/11/13.
+//
+
+import SwiftUI
+
+@main
+struct MatchingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
